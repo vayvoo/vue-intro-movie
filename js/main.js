679d61b847ba250f71a95e5ec8db54310b42025a
vayvoo <60880037+vayvoo@users.noreply.github.com>
2021-09-04 12:25:55 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        movie: {
+            title: 'John Wick 3',
+            description: 'Суперкиллер Джон Уик после нарушения кодекса тайной гильдии ассасинов получает статус изгоя – экскомьюникадо. За его голову назначена цена в 14 миллионов долларов, и армия самых жестоких профессиональных убийц со всего мира открывает на него кровавую охоту.',
+            poster: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/4ec982b3-cf7a-4589-ac69-903d786cc691/300x450',
+            alt: 'John Wick 3',
+            trailer: 'https://www.youtube.com/watch?v=x2db3fR1fBs&ab_channel=CentralPartnership'
+        }
+    }
+})
